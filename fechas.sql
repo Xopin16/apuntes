5576e6c9debf0eb3bb0414c6221ed2998b77aace
José Ignacio Rivas Fuentes <rivas.fujos20@triana.salesianos.edu>
2021-11-12 10:03:09 +0100

    fjbjhbfejhbfejh

---
+++ fechas.sql
@@ -0,0 +1,15 @@
+select '2021-11-12 09:52:10'::timestamp;
+
+select '19960907'::date;
+
+select '960907'::date;
+
+select '10:55:23.789'::time;
+
+select '1996-09-07'::date +'10:55:23'::time;
+
+select '2021-11-12 09:52:10'::timestamp +'10:55:23'::time;
+
+select current_date - '1996-09-07'::date;
+
+select '1996-09-07'::date + 10000;
